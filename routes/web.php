4ae86600967ad5763c0f43b3800e518821c42397
gaetank <gaetan.kemayou@gmail.com>
2018-06-21 14:44:58 +0200

    modification gitignore

--- routes/web.php
+++ routes/web.php
@@ -11,18 +11,10 @@
 |
 */
 
-<<<<<<< HEAD
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-=======
 Route::get('/', ['as' => 'home', function () {
     return redirect(route('movies.home'));
 }]);
 
 require_once "core/movies.php";
->>>>>>> 5a428062a620d6a9c458e1483d7795713b46a809
